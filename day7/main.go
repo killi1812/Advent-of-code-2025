840d43f355297b734ff9747f90b201604a272e8a
killi1812 <cvok.fran@gmail.com>
2025-12-07 18:35:04 +0100

    Day 7 solved

--- day7/main.go
+++ day7/main.go
@@ -16,6 +16,7 @@ const (
 
 type mainfold struct {
 	arr      [][]byte
+	cnt      [][]int
 	xLen     int
 	yLen     int
 	splitCnt int
@@ -28,6 +29,7 @@ func NewMainfold(input []string) mainfold {
 	xLen := len(input[0])
 	ret := mainfold{
 		arr:        make([][]byte, yLen),
+		cnt:        make([][]int, yLen),
 		yLen:       yLen,
 		xLen:       xLen,
 		activeLine: 0,
@@ -35,9 +37,25 @@ func NewMainfold(input []string) mainfold {
 
 	for i, line := range input {
 		ret.arr[i] = []byte(line)
+		ret.cnt[i] = make([]int, xLen)
 	}
+	ret.cnt[0] = slices.Repeat([]int{1}, xLen)
 
 	return ret
+}
+
+func (m *mainfold) Count() {
+	for _, line := range m.cnt {
+		for _, char := range line {
+			fmt.Printf("%d ", char)
+		}
+		fmt.Println()
+	}
+
+	for _, char := range m.cnt[m.yLen-2] {
+		m.splitCnt += char
+	}
+	fmt.Println()
 }
 
 func (m mainfold) Print() {
@@ -79,16 +97,23 @@ func (m *mainfold) Step() bool {
 
 func (m *mainfold) check(position int) {
 	if m.arr[m.activeLine+1][position] != SPLIT {
+		m.cnt[m.activeLine+1][position] += m.cnt[m.activeLine][position]
+
 		m.arr[m.activeLine+1][position] = PATH
 		return
 	}
 
-	m.splitCnt++
 	if position+1 < m.xLen {
+		m.cnt[m.activeLine+1][position+1] = m.cnt[m.activeLine][position] +
+			m.cnt[m.activeLine+1][position+1]
+
 		m.arr[m.activeLine+1][position+1] = PATH
 	}
 
 	if position-1 > -1 {
+		m.cnt[m.activeLine+1][position-1] = m.cnt[m.activeLine][position] +
+			m.cnt[m.activeLine+1][position-1]
+
 		m.arr[m.activeLine+1][position-1] = PATH
 	}
 }
@@ -102,9 +127,10 @@ func (m *mainfold) Go() int {
 func main() {
 	input := read()
 	m := NewMainfold(input)
-	rez := m.Go()
+	m.Go()
 
-	fmt.Printf("Result: %v\n", rez)
+	m.Count()
+	fmt.Printf("Result: %v\n", m.splitCnt)
 }
 
 func read() []string {
